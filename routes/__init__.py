52016dd5288e7c52a65edae1f8f7a030e712b517
lucia <thisislucia1991@gmail.com>
2024-07-16 10:06:25 +0800

    First Version--make some changes in the file system

---
+++ routes/__init__.py
@@ -0,0 +1,10 @@
+from fastapi import FastAPI
+from routers import api_router
+from fastapi.staticfiles import StaticFiles
+
+
+app = FastAPI()
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+app.include_router(api_router)
