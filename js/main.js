369af69f7ac735d2c77ab111ab8232f496a60eb8
lucia <thisislucia1991@gmail.com>
2024-07-17 15:31:17 +0800

    Add main.js for switch page

--- js/main.js
+++ js/main.js
@@ -1,7 +1,62 @@
 import { initWeather } from "./weather.js";
 
-//轉換天氣與雨量頁面
-const weatherPage = document.querySelector(".week_weather_info_container");
+//處理天氣與雨量頁面轉換
+function handlePageSwitch() {
+  const weatherBtn = document.getElementById("weather_btn");
+  const rainBtn = document.getElementById("rain_btn");
+  const weatherContainer = document.querySelector(
+    ".week_weather_info_and_rain_info_container"
+  );
 
-//初始化天氣資料
-document.addEventListener("DOMContentLoaded", initWeather);
+  console.log(weatherContainer);
+  const rainContainer = document.querySelector(".rain_info_container");
+
+  // 預設 weather_btn clicked
+  weatherBtn.classList.add("active");
+
+  // update display of weather and rain
+  function updateDisplay() {
+    if (weatherBtn.classList.contains("active")) {
+      weatherContainer.classList.add("show");
+      weatherContainer.classList.remove("hide");
+      rainContainer.classList.add("hide");
+      rainContainer.classList.remove("show");
+    } else if (rainBtn.classList.contains("active")) {
+      rainContainer.classList.add("show");
+      rainContainer.classList.remove("hide");
+      weatherContainer.classList.add("hide");
+      weatherContainer.classList.remove("show");
+    }
+  }
+
+  updateDisplay();
+
+  // 切換頁面
+  weatherBtn.addEventListener("click", function () {
+    this.classList.add("active");
+    rainBtn.classList.remove("active");
+    updateDisplay();
+  });
+
+  rainBtn.addEventListener("click", function () {
+    this.classList.add("active");
+    weatherBtn.classList.remove("active");
+    updateDisplay();
+  });
+}
+
+function initPage() {
+  //載入地圖
+
+  // 載入天氣頁面
+  initWeather();
+
+  //載入雨量頁面
+  // getRainData();
+
+  //監聽頁面轉換按鈕
+  handlePageSwitch();
+}
+
+// //初始化天氣資料
+document.addEventListener("DOMContentLoaded", initPage);
